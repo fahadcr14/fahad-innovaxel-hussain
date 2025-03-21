77df4861d70a86895f70c9889ded31c206c33d97
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2025-03-21 15:11:44 +0500

    developed-url-shorten-post-api

--- app.py
+++ app.py
@@ -14,3 +14,23 @@ def db_init():
     db_cursor.execute("CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, short_url TEXT UNIQUE, original_url TEXT, access_count INTEGER DEFAULT 0)")
     db_conn.commit()
     db_conn.close()
+
+
+
+def generate_short_url(original_url):
+    """Generates a short URL"""
+    return hashlib.md5(original_url.encode()).hexdigest()[:6]
+
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    """Shortens a URL"""
+    data = request.get_json()
+    original_url = data['url']
+    short_url = generate_short_url(original_url)
+    db_conn=sqlite3.connect('urls.db')
+    db_cursor=db_conn.cursor()
+    db_cursor.execute("INSERT INTO urls (short_url, original_url) VALUES (?, ?)", (short_url, original_url))
+    db_conn.commit()
+    db_conn.close()
+    return jsonify({'short_url': short_url})
