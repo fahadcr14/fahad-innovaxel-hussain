383efcc675f6900124044c62261ff4a01449469f
fahadcr14 <bsdsf21m014@pucit.edu.pk>
2025-03-22 12:28:03 +0500

    api testing fixed minor bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect, url_for,render_template
+from flask import Flask, request, jsonify, redirect,render_template
 import hashlib
 import sqlite3
 
@@ -13,21 +13,23 @@ def db_init():
         db_cursor=db_conn.cursor()
         db_cursor.execute("""
             CREATE TABLE IF NOT EXISTS urls (
-                short_url TEXT PRIMARY KEY,
+                shortCode TEXT PRIMARY KEY,
                 original_url TEXT NOT NULL,
-                access_count INTEGER DEFAULT 0
+                access_count INTEGER DEFAULT 0,
+                createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         """)
-        db_cursor.execute("""
+        db_conn.execute("""
         CREATE INDEX IF NOT EXISTS idx_shortCode ON urls (shortCode)
     """)
         db_conn.commit()
-        db_conn.close()
+ 
   
 
 
 
-def generate_short_url(original_url):
+def generate_shortCode(original_url):
     """
     Generates a short URL
     :param original_url: The original URL
@@ -38,20 +40,29 @@ def generate_short_url(original_url):
     """
     return hashlib.md5(original_url.encode()).hexdigest()[:6]
 
-
+def is_url_exist(db_cursor,original_url):
+    """
+    Check if the URL already exists in the database
+    """
+    with get_db_connection() as db_conn:
+        db_cursor=db_conn.cursor()
+        db_cursor.execute("SELECT shortCode FROM urls WHERE original_url = ?", (original_url,))
+        existing_shortCode = db_cursor.fetchone()
+        if existing_shortCode:
+            return existing_shortCode[0]
+        return None
 @app.route('/shorten', methods=['POST'])
 def shorten():
     """Shortens a URL"""
     data = request.get_json()
     original_url = data['url']
-    short_url = f"{generate_short_url(original_url)}"
+    shortCode = f"{generate_shortCode(original_url)}"
     with get_db_connection() as db_conn:
         db_cursor=db_conn.cursor()
         try: 
-            db_cursor.execute("SELECT short_url FROM urls WHERE original_url = ?", (original_url,))
-            existing_short_url = db_cursor.fetchone()
-            if existing_short_url:
-                return jsonify({'short_url': existing_short_url[0]})
+            existing_shortCode = is_url_exist(db_cursor,original_url)
+            if existing_shortCode:
+                return jsonify({'shortCode': existing_shortCode[0]})
         except sqlite3.Error as e:
             return jsonify({'error': str(e)}), 500
         attempt=0
@@ -59,12 +70,12 @@ def shorten():
             if attempt>10:
                 return jsonify({'error': 'Failed to shorten the URL'}), 500
             try:
-                db_cursor.execute("INSERT INTO urls (short_url, original_url) VALUES (?, ?)", (short_url, original_url))
+                db_cursor.execute("INSERT INTO urls (shortCode, original_url) VALUES (?, ?)", (shortCode, original_url))
                 db_conn.commit()
-                return jsonify({'short_url': short_url})
+                return jsonify({'shortCode': shortCode})
             except sqlite3.IntegrityError:
                 attempt+=1
-                short_url = f"{generate_short_url(original_url)}"
+                shortCode = f"{generate_shortCode(original_url)}"
                 continue
             
         
@@ -75,13 +86,12 @@ def redirect_to_original(shorten_url):
     """Redirects to the original URL"""
     with get_db_connection() as db_conn:
         db_cursor=db_conn.cursor()
-        db_cursor.execute("SELECT original_url FROM urls WHERE short_url = ?", (shorten_url,))
+        db_cursor.execute("SELECT original_url FROM urls WHERE shortCode = ?", (shorten_url,))
         original_url = db_cursor.fetchone()
         if original_url:
-            db_cursor.execute("UPDATE urls SET access_count = access_count + 1 WHERE short_url = ?", (shorten_url,))
+            db_cursor.execute("UPDATE urls SET access_count = access_count + 1 WHERE shortCode = ?", (shorten_url,))
             db_conn.commit()
             return redirect(original_url[0])
-        db_conn.close() #closing the connection
         return jsonify({'error': 'URL not found'}), 404
 
 
@@ -90,17 +100,15 @@ def get_stats_of_url(shorten_url):
     """
     Get the stats of a URL
     Takes the short url from url slug and returns the access count of the URL
-
-    
     """
     with get_db_connection() as db_conn:
         db_cursor=db_conn.cursor()
-        db_cursor.execute("SELECT * FROM urls WHERE short_url = ?", (shorten_url,))
+        db_cursor.execute("SELECT * FROM urls WHERE shortCode = ?", (shorten_url,))
         row_data = db_cursor.fetchone()
+        print(row_data)
+
         if row_data:
-            db_conn.close()
-            return jsonify(dict(row_data))
-        db_conn.close()
+            return jsonify({'shortCode': row_data[0], 'original_url': row_data[1], 'access_count': row_data[2], 'createdAt': row_data[3], 'updatedAt': row_data[4]})
         return jsonify({'error': 'URL not found'}), 404
 
 @app.route('/<shorten_url>/', methods=["DELETE"])
@@ -112,9 +120,8 @@ def delete_url(shorten_url):
     db_conn=sqlite3.connect('urls.db')
     db_cursor=db_conn.cursor()
     try:
-        db_cursor.execute("DELETE FROM urls WHERE short_url = ?", (shorten_url,))
+        db_cursor.execute("DELETE FROM urls WHERE shortCode = ?", (shorten_url,))
         db_conn.commit()
-        db_conn.close()
 
         return  jsonify({'message': 'URL deleted'}),204
     except sqlite3.Error as e:
@@ -131,9 +138,10 @@ def update_url(shorten_url):
     with get_db_connection() as db_conn:
         db_cursor=db_conn.cursor
         try:
-            db_cursor.execute("UPDATE urls SET original_url = ? WHERE short_url = ?", (new_url, shorten_url))
+            if not is_url_exist(db_cursor,new_url):
+                return jsonify({'error': 'URL not exists'}),404
+            db_cursor.execute("UPDATE urls SET original_url = ? WHERE shortCode = ?", (new_url, shorten_url))
             db_conn.commit()
-            db_conn.close()
             return jsonify({'message': 'URL updated'})
         except sqlite3.Error as e:
             return jsonify({'error': str(e)}), 500
@@ -141,6 +149,7 @@ def update_url(shorten_url):
 @app.route("/")
 def index():
      return render_template('index.html')
+
 if __name__ == '__main__':
     db_init()
     app.run()
